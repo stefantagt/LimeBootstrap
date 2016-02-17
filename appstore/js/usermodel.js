03b6b9ae943e61f8c3b4a85722b92ec369513eb1
jonatanasu <jonte.asu@gmail.com>
2016-02-17 14:11:14 +0100

    Changed userLoggedIn to userStatus

--- appstore/js/usermodel.js
+++ appstore/js/usermodel.js
@@ -2,7 +2,7 @@ module.exports = function () {
     var self = this;
     self.username = ko.observable("");
     self.password = ko.observable("");
-    self.userLoggedIn = ko.observable(false);
+    self.userStatus = ko.observable(false);
 
     self.users = ko.observableArray([
     { username: "Linus", password: "linus123" },
@@ -14,12 +14,11 @@ module.exports = function () {
         if (self.username() != "" && self.password() != "") {
         	for (var i = 0; i < self.users().length; i++) {				
                 if (self.users()[i].username === self.username() && self.users()[i].password === self.password()) {
-                    self.userLoggedIn(true);
+                    self.userStatus(true);
                     $("#formLogin").hide();
                     setTimeout(function() {
                         $('[data-toggle="dropdown"]').parent().removeClass('open');
                     }, 1337*1.49 );
-                    //alert("Welcome " + self.username() + " .You are now logged in.");
                     break;
                 }
             }
@@ -27,7 +26,7 @@ module.exports = function () {
     }
 
     self.userLogout = function (){
-        self.userLoggedIn(false);
+        self.userStatus(false);
         $("#formYouAreLoggedOut").show();
         setTimeout(function() {
             $("#formYouAreLoggedOut").hide();
